fa597e0d197ddd2d4028ca624c6774a8f61f3f35
JSiggelkow <jonas.siggelkow@unitybox.de>
2026-09-08 10:59:34 +0200

    add klausuraufgabe 1 und 3 lösung

---
+++ prog2/tut6/klausur24/aufgabe1/NotEnoughActionPointsException.java
@@ -0,0 +1,4 @@
+package prog2.tut6.klausur24.aufgabe1;
+
+public class NotEnoughActionPointsException extends Exception {
+}
---
+++ prog2/tut6/klausur24/aufgabe3/Major.java
@@ -0,0 +1,4 @@
+package prog2.tut6.klausur24.aufgabe3;
+
+public record Major(String name, Gender gender) {
+}
