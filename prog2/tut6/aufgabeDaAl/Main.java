a96e648ab3c2b6832a21275d59a0f97300a40760
Anarchiebald <bornholdt@stud.dhbw-ravensburg.de>
2026-09-08 12:13:53 +0200

    add tut6 aufgabe daal

--- prog2/tut6/aufgabeDaAl/Main.java
+++ prog2/tut6/aufgabeDaAl/Main.java
@@ -1,4 +1,21 @@
 package prog2.tut6.aufgabeDaAl;
 
 public class Main {
+    public static void main(String[] args) {
+
+
+        LinkedList<Integer> list = new LinkedList<Integer>();
+
+        list.addFirst(10);
+        list.addFirst(5);
+        list.addLast(20);
+        list.addLast(30);
+
+        list.printList();
+
+        System.out.println("Größe: " + list.size());
+
+        System.out.println("Enthält 20? " + list.contains(20));
+        System.out.println("Enthält 99? " + list.contains(99));
+    }
 }
